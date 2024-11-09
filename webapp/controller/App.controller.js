f9a9c101d2f6e9b711647314884ce847132d45d3
ray <arthurwanggang@yeah.net>
2024-11-09 19:05:18 +0800

    feat: init project

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define([
+	"sap/ui/Device",
+	"sap/ui/core/mvc/Controller",
+	"sap/base/strings/formatMessage"
+], (Device, Controller) => {
+	"use strict";
+
+	return Controller.extend("sap.ui.demo.todo.controller.App", {
+
+		onInit() {
+		},
+	});
+
+});
